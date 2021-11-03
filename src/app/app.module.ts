ff7da6c849fb638618a60c4bbb34b65aecc87949
lekyvong09 <lekyvong09@gmail.com>
2021-11-03 20:41:19 +0700

    Angular - clean up for eCommerce project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SalesPersonListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
