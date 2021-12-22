6581be6c63d3d86f9c126fa9fb4b9334ac8828b3
lekyvong09 <lekyvong09@gmail.com>
2021-12-22 21:28:39 +0700

    Security - user-management 3

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
-import { faPlus, faSync, faEdit, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faSync, faEdit, faTrash, faUser, faLock, faUnlock, faIdBadge, faEnvelope, faShieldAlt, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import {UserService} from '../_service/user.service';
 import {Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {User} from '../_model/user';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user',
@@ -12,13 +13,14 @@ import {User} from '../_model/user';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit, OnDestroy {
-  faUser = faUser; faPlus = faPlus; faSync = faSync; faEdit = faEdit; faTrash = faTrash;
+  faUser = faUser; faPlus = faPlus; faSync = faSync; faEdit = faEdit; faTrash = faTrash; faLock = faLock; faUnlock = faUnlock;
+  faIdBadge = faIdBadge; faEnvelope = faEnvelope; faShieldAlt = faShieldAlt; faSignInAlt = faSignInAlt
 
   users: User[];
   showLoading: boolean;
   private subscriptions: Subscription[] = [];
 
-  constructor(private userService: UserService, private router: Router, private toastr: ToastrService) { }
+  constructor(private userService: UserService, private router: Router, private toastr: ToastrService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.getUsers();
@@ -31,6 +33,12 @@ export class UserComponent implements OnInit, OnDestroy {
           this.users = response;
         }, error => this.toastr.error(error.error.message)
     );
+  }
+
+  openUserInfo(content): void {
+    this.modalService.open(content).result.then(
+        (resultonFulfilled) => console.log(resultonFulfilled ? resultonFulfilled : 'save'),
+        (reasonOnReject) => console.log(reasonOnReject ? reasonOnReject : 'cancel'));
   }
 
   ngOnDestroy(): void {
