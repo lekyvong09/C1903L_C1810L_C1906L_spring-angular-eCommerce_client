0278d97a570f0802b0b81f713f912c379b2433e2
lekyvong09 <lekyvong09@gmail.com>
2021-12-22 19:46:41 +0700

    Security - register

--- src/app/login-status/login-status.component.ts
+++ src/app/login-status/login-status.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from '../_service/authentication.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-login-status',
@@ -11,13 +13,15 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated = false;
   userFullName: string;
 
-  constructor(public authenticationService: AuthenticationService) { }
+  constructor(public authenticationService: AuthenticationService, private router: Router, private toastr: ToastrService ) { }
 
   ngOnInit(): void {
   }
 
   logout(): void {
     this.authenticationService.logOut();
+    this.router.navigate(['/login']).then(r => {});
+    this.toastr.info('You have logged out successfully');
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthenticationService} from '../_service/authentication.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {Subscription} from 'rxjs';
+import {finalize} from 'rxjs/operators';
+import {User} from '../_model/user';
+import {faUser, faIdCard, faEnvelope, faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  faUser = faUser;
+  faIdCard = faIdCard;
+  faEnvelope = faEnvelope;
+  faSpinner = faSpinner;
+  showLoading: boolean;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private router: Router, private  toastr: ToastrService) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.isLoggedIn()) {
+      this.router.navigateByUrl('/user/management').then(r => {});
+    }
   }
+
+  onRegister(user: any): void {
+    this.showLoading = true;
+    console.log(user);
+    this.authenticationService.register(user)
+        .pipe(finalize(() => this.showLoading = false))
+        .subscribe((response: User) =>
+            {this.router.navigateByUrl('/login')
+                  .then(r => this.toastr.success(`A new account was created for ${response.firstName}. Please check your email`));
+            }, error => this.toastr.error(error.error.message)
+        );
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
 
 }
