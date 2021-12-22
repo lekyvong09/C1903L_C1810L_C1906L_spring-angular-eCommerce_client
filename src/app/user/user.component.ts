3a1cbfe854bea00236aac3a8eb6a45371fd262e3
lekyvong09 <lekyvong09@gmail.com>
2021-12-22 21:03:48 +0700

    Security - user-management 2

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import { faPlus, faSync, faEdit, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import {UserService} from '../_service/user.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {User} from '../_model/user';
 
 @Component({
   selector: 'app-user',
@@ -10,12 +14,23 @@ import { faPlus, faSync, faEdit, faTrash, faUser } from '@fortawesome/free-solid
 export class UserComponent implements OnInit, OnDestroy {
   faUser = faUser; faPlus = faPlus; faSync = faSync; faEdit = faEdit; faTrash = faTrash;
 
+  users: User[];
   showLoading: boolean;
   private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(
+        (response: User[]) => {
+          this.userService.addUsersToLocalCache(response);
+          this.users = response;
+        }, error => this.toastr.error(error.error.message)
+    );
   }
 
   ngOnDestroy(): void {
